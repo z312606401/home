484c65c300eab738fd46b0a79c2aa6bfefb6dfa3
zjy <zjy@DESKTOP-KVVI2CQ>
2016-07-23 20:14:35 +0800

    2

--- git/src/git/heo2.java
+++ git/src/git/heo2.java
@@ -1,11 +1,10 @@
 package git;
 
-public class heo {
+public class heo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("hahah");
+
 	}
 
 }
